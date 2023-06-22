fd6223791e987b061f66aacb317e96571cccd08b
Ajay <aji478514@gmail.com>
2023-06-22 14:34:46 +0530

    admin login featured

--- config/collection.js
+++ config/collection.js
@@ -4,5 +4,6 @@ module.exports={
     PRODUCT:'product',
     USER_COLLECTION:'user',
     Cart_Collection:'cart',
-    Order_collection:'orders'
+    Order_collection:'orders',
+    Admin_Data:'admin'
 }
